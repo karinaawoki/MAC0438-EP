5a1ba516de6f85752ec76c1eee0689ba7257eb62
karina <k.sueminha@gmail.com>
2015-04-18 20:02:08 -0300

    inicializando as posições

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 #include <pthread.h>
 
 DEFINE SHARED 1
+
+sem_t pista, mutex;
+int numBikes;
+int *bikesPorPista;
+
+
 void *ciclista()
 {
 
@@ -20,26 +26,41 @@ int main(int argc, char*argv[])
 }
 
 
-int corrida(int n)
+int iniciaCorrida(int n, int d)
 {
+	int result_code;
+   	int i, r;
    	pthread_t threads[n];
-   	int thread_args[n];
-   	int result_code, i;
- 	sem_t *pista;
+	int thread_args[n];
 
-	pista = malloc(n*sizeof(sem_t));
+	/*Temos zero bikes em cada pista, inicialmente*/
+	bikesPorPista = malloc(d*sizeof(int));
+	for (i = 0; i < d; i++)
+	{
+		bikesPorPista[i] = 0;
+	}
+
 	/* Semaforos inicializados com 4 */
+	pista = malloc(n*sizeof(sem_t));
     for(i = 0; i < n; i++){
-    	sem_init(pista[i],SHARED,4);
+    	sem_init(pista[i], SHARED, 4);
     }
 
-   	/* create all threads one by one */
-   	for (i = 1; i < n; i++) {
 
+    /* LARGADA */
+    for(i = 0; i<n; i++)
+    {
+    	while(bikesPorPista[r=((int)rand()%d)] == 4);
+    	bikesPorPista[r]--;
+    	sem_wait(pista[d]);
+    }
+
+
+   	for (i = 1; i < n; i++) {
     	/*Passa para cada thread o seu respectivo número -- ACHO??? */
     	thread_args[i] = i;
 
-    	/* Criand threads */
+    	/* Criando n threads */
       	result_code = pthread_create(&threads[i], NULL, ciclista, (void *) &thread_args[i]);
       	
       	if (result_code != 0)
