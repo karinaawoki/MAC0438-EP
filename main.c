2f2023fcce99275d087ec2ff7b852a4fad05f020
karina <k.sueminha@gmail.com>
2015-04-19 15:21:52 -0300

    quase compilando

--- main.c
+++ main.c
@@ -2,34 +2,29 @@
 #define SHARED 1
 
 #include <stdio.h>
-#include <stdin.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 
-
+/* VARIÁVEIS GLOBAIS */
 sem_t pista, mutex;
 int numBikes;
 int *bikesPorPista;
-
 pthread_barrier_t barrera; 
 
 
-void *ciclista(){
-  /*
-    faz coisas
-  */
+/* FUNÇÕES */
+int iniciaCorrida(int n, int d);
+void *ciclista();
 
 
-  pthread_barrier_wait(&barrera);
-
-}
 
 int main(int argc, char*argv[]){
   /* d n [v/u] */
 
-  int d = argv[1];
-  int n = argv[2];
+  int d = atoi(argv[1]);
+  int n = atoi(argv[2]);
 
   iniciaCorrida(n,d);
 
@@ -37,7 +32,19 @@ int main(int argc, char*argv[]){
 }
 
 
-int iniciaCorrida(int n, int d){
+void *ciclista()
+{
+  /*
+    faz coisas
+  */
+
+  pthread_barrier_wait(&barrera);
+  return NULL;
+}
+
+
+int iniciaCorrida(int n, int d)
+{
   int result_code;
   int i, r;
   pthread_t threads[n];
@@ -47,30 +54,34 @@ int iniciaCorrida(int n, int d){
 
   /*Temos zero bikes em cada pista, inicialmente*/
   bikesPorPista = malloc(d*sizeof(int)); 
-  for (i = 0; i < d; i++){
+  for (i = 0; i < d; i++)
+  {
       bikesPorPista[i] = 0;
-    }
+  }
 
   /* Semaforos inicializados com 4 */
   pista = malloc(n*sizeof(sem_t));
-  for(i = 0; i < n; i++){
+  for(i = 0; i < n; i++)
+  {
     sem_init(pista[i], SHARED, 4);
   }
 
 
   /* LARGADA */
-  for(i = 0; i<n; i++){
+  for(i = 0; i<n; i++)
+  {
       while(bikesPorPista[r=((int)rand()%d)] == 4);
       bikesPorPista[r]++;
       sem_wait(pista[d]);
-    }
+  }
 
 
-  for(i = 1; i < n; i++){
+  for(i = 1; i < n; i++)
+  {
     thread_args[i] = i;
 
     /* Criando n threads */
     if(pthread_create(&threads[i], NULL, ciclista,NULL)) abort();
   }
-
+  return NULL;
 }
